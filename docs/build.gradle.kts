8949e2d4aa98f7716c08796403bf66ac3cb8ee3f
Nico D'Cotta <nico.dc@outlook.com>
2022-04-13 19:57:23 +0100

    use multiversion docs

--- docs/build.gradle.kts
+++ docs/build.gradle.kts
@@ -1,31 +1,8 @@
-
-// import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
-
 plugins {
     id("ru.vyarus.mkdocs") version "2.3.0"
 }
 
-/*
-val generatedDocs = file("src/doc/docs/generated")
+with(mkdocs.publish) {
+    docPath
 
-tasks.clean {
-    delete(generatedDocs)
-}
-
-val copyFromDokka by tasks.registering(Copy::class) {
-    val dokkaTask = rootProject.tasks.named<DokkaMultiModuleTask>("dokkaHtmlMultiModule")
-    dependsOn(dokkaTask)
-
-    from(dokkaTask.get().outputDirectory)
-    into(generatedDocs)
-}
-
-tasks.withType<MkdocsTask> {
-    dependsOn(copyFromDokka)
-}
-*/
-
-python {
-    // modules.removeIf { "mkdocs-material" in it }
-    // modules = modules + "mkdocs-material:7.1.6"
 }
