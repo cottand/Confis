7e36055937a876c74f83b88099a6d5134c91fdec
Nico D'Cotta <nico.dc@outlook.com>
2022-04-16 15:26:14 +0100

    remove example main function

--- scriptHost/src/main/kotlin/eu/dcotta/confis/scripting/host.kt
+++ scriptHost/src/main/kotlin/eu/dcotta/confis/scripting/host.kt
@@ -3,7 +3,6 @@ package eu.dcotta.confis.scripting
 import java.io.File
 import kotlin.script.experimental.api.EvaluationResult
 import kotlin.script.experimental.api.ResultWithDiagnostics
-import kotlin.script.experimental.api.ScriptDiagnostic
 import kotlin.script.experimental.host.toScriptSource
 import kotlin.script.experimental.jvm.dependenciesFromCurrentContext
 import kotlin.script.experimental.jvm.jvm
@@ -22,19 +21,6 @@ fun evalFile(scriptFile: File): ResultWithDiagnostics<EvaluationResult> {
     return BasicJvmScriptingHost().eval(scriptFile.toScriptSource(), compilationConfiguration, null)
 }
 
-fun main(vararg args: String) {
-    if (args.size != 1) {
-        println("usage: <app> <script file>")
-    } else {
-        val scriptFile = File(args[0])
-        println("Executing script $scriptFile")
-
-        val res = evalFile(scriptFile)
-
-        res.reports.forEach {
-            if (it.severity > ScriptDiagnostic.Severity.DEBUG) {
-                println(" : ${it.message}" + if (it.exception == null) "" else ": ${it.exception}")
-            }
-        }
-    }
+fun main() {
+    TODO()
 }
