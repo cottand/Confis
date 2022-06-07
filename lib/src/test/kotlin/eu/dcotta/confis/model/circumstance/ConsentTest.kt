c0ffb9e44f3f97426bb9feac27dfd71ccaa0a28b
Nico D'Cotta <nico.dc@outlook.com>
2022-06-07 11:56:35 +0100

    add consent tests

--- lib/src/test/kotlin/eu/dcotta/confis/model/circumstance/ConsentTest.kt
+++ lib/src/test/kotlin/eu/dcotta/confis/model/circumstance/ConsentTest.kt
@@ -4,8 +4,11 @@ import eu.dcotta.confis.Sentence
 import eu.dcotta.confis.model.CircumstanceMap
 import eu.dcotta.confis.model.Obj
 import eu.dcotta.confis.model.Party
+import eu.dcotta.confis.model.circumstance.Purpose.Commercial
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
 
 class ConsentTest : StringSpec({
     val alice = Party("alice")
@@ -15,6 +18,9 @@ class ConsentTest : StringSpec({
     val aliceEatsCookie = Sentence { "alice"("pay", Obj("cookie")) }
 
     "can have several consents in a circumstance map" {
+        (Consent(aliceEatsCookie, from = bob) generalises PurposePolicy(Commercial)) shouldBe false
+        (PurposePolicy(Commercial) generalises (Consent(aliceEatsCookie, from = bob))) shouldBe false
+
         val small = CircumstanceMap.of(Consent(alicePaysBob, from = bob))
         val large = CircumstanceMap.of(Consent(alicePaysBob, from = alice)) + small
 
@@ -29,5 +35,21 @@ class ConsentTest : StringSpec({
 
         (cookie generalises paying) shouldBe false
         (paying generalises cookie) shouldBe false
+    }
+
+    "consent from different peple does not generalise" {
+        val fromBob = Consent(alicePaysBob, from = bob)
+        val fromAlice = Consent(alicePaysBob, from = alice)
+
+        (fromBob generalises fromAlice) shouldBe false
+    }
+
+    "render" {
+        val fromBob = Consent(alicePaysBob, from = bob)
+
+        fromBob.render() should {
+            it shouldContain "with the consent from"
+            it shouldContain bob.render()
+        }
     }
 })
