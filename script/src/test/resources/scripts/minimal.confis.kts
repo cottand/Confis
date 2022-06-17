5cfa2a6adc6ae1e2d913c46f08df724631869478
Nico D'Cotta <nico.dc@outlook.com>
2022-06-17 14:01:42 +0100

    fix minimal example

--- script/src/test/resources/scripts/minimal.confis.kts
+++ script/src/test/resources/scripts/minimal.confis.kts
@@ -1,19 +1,7 @@
-
-
 title = "Minimal example"
 
 val alice by party
 val eat by action
-val soup by thing
+val cookie by thing
 
-val cake by thing
-
-alice mayNot  eat(soup) unless  {
-    with purpose Commercial
-    after { alice did eat(cake) }
-}
-
-alice may eat(soup) asLongAs {
-    with purpose Commercial
-    after { alice did eat(cake) }
-}
+alice may eat(cookie)
